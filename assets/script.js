3cebbed62074f4dca09f6b6541f4bb16b16b19ed
claytontsnell@gmail.com <claytontsnell@gmial.com>
2022-11-29 07:44:10 -0500

    finishing up

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // var city = $("#city")
 var apiKey = "27002608777aef815297df3644b96c02"
 
-var url = `api.openweathermap.org/data/2.5/forecast?lat=40N&lon=111W&appid=${APIkey}`
+var url = `api.openweathermap.org/data/2.5/forecast?lat=40N&lon=111W&appid=${apiKey}`
  
 // var geourl = "api.openweathermap.org/data/2.5/weather?q=SaltLake&appid=27002608777aef815297df3644b96c02"
 
@@ -9,9 +9,9 @@ var cityInput = document.querySelector('#cityInput');
 var searchBtn = document.querySelector('#searchBtn');
 var searchHistoryEl = document.querySelector("#searchHistory");
 
+// api.openweathermap.org/data/2.5/weather?q=SaltLake&appid=27002608777aef815297df3644b96c02
 
 // get weather
-
 searchBtn.addEventListener('click', function(event) {
   event.preventDefault();
 
@@ -44,7 +44,7 @@ function save() {
   dividerEl.className = "searchDivider";
 }
 
-//Creates buttons from storage
+//  creates buttons from storage
 function getButtons() {
   
   if(localStorage.getItem('Cities:') != null) {
@@ -61,7 +61,7 @@ function getButtons() {
     }
   } 
 
-  //Render city's forecast on click
+  // render weather function
   function renderWeather() {
     searchItemBtn.addEventListener("click", function(event) { 
       event.preventDefault();
@@ -74,7 +74,7 @@ function getButtons() {
   }
 }
 
-//KEEP BUTTONS ON RELOAD
+// saves old searches
 function keepButtons () {
   if (localStorage === "") {
 
@@ -84,9 +84,9 @@ function keepButtons () {
 }
 
 
-//GET WEATHER FUNCTION
+//  function that calls weather api 
 function getWeather(city) {
-    //Geo location to get latitude and longitude for "One Call"
+     //Geo location 
     var cityLatLonURL ='https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=' + apiKey;
     fetch(cityLatLonURL)
       .then(function (response) {
@@ -106,6 +106,105 @@ function getWeather(city) {
         cityName.innerHTML = cityData;
         todayContainerEl.append(cityName);
 
+        //"ONE CALL" FOR CURRENT & 5-DAY FORECAST
+        var weatherURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + '&units=imperial';
+        fetch(weatherURL)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
 
+          //Current Date
+          var todayDate = document.createElement('h3');
+          var currentDate = moment.unix(data.current.dt).format(" (MM/DD/YYYY) ");
+          todayDate = currentDate;
+          cityName.append(todayDate);
+        
+          //Current Weather Icon
+          var imgEl = document.createElement('img');
+          var iconCode = data.current.weather[0].icon;
+          var iconUrl = "https://openweathermap.org/img/w/" + iconCode + ".png";
+          imgEl.setAttribute('src', iconUrl);
+          cityName.append(imgEl);
+       
+          //Current Temperature
+          var tempToday = document.createElement('p');
+          var currentTemp = data.current.temp;
+          tempToday.innerHTML = "Temperature: " + currentTemp + "\u00B0" + " F";
+          todayContainerEl.append(tempToday);
+    
+          //Current Wind Speed 
+          var windToday = document.createElement('p');
+          var currentWind = data.current.wind_speed;
+          windToday.innerHTML = "Wind: " + currentWind + " MPH";
+          todayContainerEl.append(windToday);
+
+          //Current Humidity
+          var humidityToday = document.createElement('p');
+          var currentHumidity = data.current.humidity;
+          humidityToday.innerHTML = "Humidity: " + currentHumidity + "%";
+          todayContainerEl.append(humidityToday);
+
+          //Current UV Index
+          var uvIndexLabel = document.createElement('p');
+          todayContainerEl.append(uvIndexLabel);
+          uvIndexLabel.innerHTML = "UV Index:   " 
+
+          var uvIndexValue = document.createElement('p');
+          uvIndexLabel.append(uvIndexValue);
+
+          var currentUVI = data.current.uvi;
+          uvIndexValue.innerHTML = currentUVI;
+          
+          if (currentUVI <= 2) {
+            $(uvIndexValue).addClass("favorable");
+          } else if (currentUVI >= 3 && currentUVI <= 5) {
+            $(uvIndexValue).addClass("moderate");
+          } else {
+            $(uvIndexValue).addClass("severe");
+          }
+
+          // forcast title
+          var fiveDayTitleEl = document.querySelector("#fiveDayTitle");
+          fiveDayTitleEl.innerHTML = "5-Day Forecast:";
+
+          for (var i = 0; i < 5; i++) {
+
+          // forcast Cards
+          var cardRowEl = document.querySelector("#cardRow");
+          var cardEl = document.createElement('div');
+          cardEl.classList.add("custom-card");
+          cardEl.classList.add("col");
+          cardRowEl.append(cardEl);
+          //date
+          var dateEl = document.createElement('div');
+          var dateData = moment.unix(data.daily[i].dt).format("MM/DD/YYYY")
+          dateEl.innerHTML = dateData;
+          dateEl.classList.add("custom-header");
+          cardEl.append(dateEl);
+          //icon
+          var imgEl5 = document.createElement('img');
+          var iconCode5 = data.daily[i].weather[0].icon;
+          var iconUrl5 = "https://openweathermap.org/img/w/" + iconCode5 + ".png";
+          imgEl5.setAttribute('src', iconUrl5);
+          cardEl.append(imgEl5);
+          //temp
+          var temp = document.createElement('p');
+          var tempData = data.daily[i].temp.day
+          temp.innerHTML = "Temp: " + tempData + "\u00B0" + " F";
+          cardEl.append(temp);
+          //wind
+          var wind = document.createElement('p');
+          var windData = data.daily[i].wind_speed
+          wind.innerHTML = "Wind: " + windData + " MPH";
+          cardEl.append(wind);
+          //humidity
+          var humidity = document.createElement('p');
+          var humidityData = data.daily[i].humidity
+          humidity.innerHTML = "Humidity: " + humidityData + "%";
+          cardEl.append(humidity);
+          }
+
+        });
       }); 
-    }
+}
